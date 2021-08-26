6f604ddec12742cc3884ea58e642c2c9490bbeb9
seyidayo <faithid10@gmail.com>
2021-08-26 17:04:49 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "more-sleep-web-app",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-react-helmet",
+  ],
+};
